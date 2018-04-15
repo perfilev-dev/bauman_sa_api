5d65299125272feb9599f6b45d9a5cc0231ef015
perfilev <perfilev@Sergeys-MacBook-Pro.local>
2018-04-15 10:44:37 +0300

    Initial commit.

--- api.py
+++ api.py
@@ -34,6 +34,7 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--incognito")
+options.add_argument("--no-sandbox")
 
 driver = webdriver.Chrome(chrome_options=options)
 
